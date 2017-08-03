c5cd734073e53c1ad9f4aa314c0ecf6320897b22
amalakamala <amalakamala.a@gmail.com>
2017-08-03 14:08:17 -0400

    nav

---
+++ assets/js/main.js
@@ -0,0 +1,20 @@
+$(document).ready(function() {
+	$('.carousel.carousel-slider').carousel({fullWidth: true});
+
+	$('.carousel').carousel();
+		setInterval(function() {
+		$('.carousel').carousel('next');
+	}, 3000);
+
+	$('.button-collapse').sideNav({
+    	menuWidth: 300, // Default is 300
+    	edge: 'left', 
+    	closeOnClick: true, // Closes side-nav on <a> clicks, useful for Angular/Meteor
+    	draggable: true, // Choose whether you can drag to open on touch screens,
+    });
+
+    //scroll spy
+    $('.scrollspy').scrollSpy();
+    //smoove transition
+    $('.smoo').smoove({opacity:'0', moveY: '90px',});
+});
