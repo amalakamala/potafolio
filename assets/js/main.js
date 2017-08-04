33932e10e06a390e7e6c0b7f376b6a79217ea9bb
amalakamala <amalakamala.a@gmail.com>
2017-08-03 23:10:49 -0400

    portafolio

--- assets/js/main.js
+++ assets/js/main.js
@@ -17,4 +17,38 @@ $(document).ready(function() {
  
     //smoove transition
     $('.mov').smoove({opacity:'0', moveY: '20px',});
+
+
+	$("#port-id-img-1").hover(function(){
+		$("#port-1").removeClass('hide',1000);}, 
+		function(){$("#port-1").addClass('hide',1000);}
+	);
+	$("#port-id-img-2").hover(function(){
+	    $("#port-2").removeClass('hide',1000);}, 
+	    function(){$("#port-2").addClass('hide',1000);}
+	);
+	$("#port-id-img-3").hover(function(){
+	    $("#port-3").removeClass('hide',1000);}, 
+	    function(){$("#port-3").addClass('hide',1000);}
+	);
+	$("#port-id-img-4").hover(function(){
+	    $("#port-4").removeClass('hide',1000);}, 
+	    function(){$("#port-4").addClass('hide',1000);}
+	);
+	$("#port-id-img-5").hover(function(){
+	    $("#port-5").removeClass('hide',1000);}, 
+	    function(){$("#port-5").addClass('hide',1000);}
+	);	
+	$("#port-id-img-6").hover(function(){
+	    $("#port-6").removeClass('hide',1000);}, 
+	    function(){$("#port-6").addClass('hide',1000);}
+	);	
+	$("#port-id-img-7").hover(function(){
+	    $("#port-7").removeClass('hide',1000);}, 
+	    function(){$("#port-7").addClass('hide',1000);}
+	);	
+	$("#port-id-img-8").hover(function(){
+	    $("#port-8").removeClass('hide')}, 
+	    function(){$("#port-8").addClass('hide');}
+	);			
 });
